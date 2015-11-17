a2d303ecd749e41d6f3a5af2392f6e1c176368ae
Cojomax99 <cojomax99@gmail.com>
2015-11-16 19:39:13 -0500

    Moved templates into partials, styles to stylesheet

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,5 @@
-
-var app = require('express')();
+var express = require('express');
+var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
@@ -17,3 +17,5 @@ io.on('connection', function(socket){
 http.listen(3000, function(){
     console.log('listening on *:3000');
 });
+
+app.use(express.static('public'));
